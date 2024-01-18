936946596d1448299e1dea620a9dc82f04db78ba
Jason Huras <hurasj@uoguelph.ca>
2024-01-18 13:31:19 -0500

    Updates

--- src/components/Login.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material"
-
-export default function Login() {
-    return (
-        <Typography variant="h1" sx={{ textAlign: 'right', paddingRight: '40px' }} >
-            Login
-        </Typography>
-    )
-}
